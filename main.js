8861d7f12936546c5eb88df27b10f2898cd4029d
Ezoza <Ezoza@.ru>
2021-12-21 14:38:47 +0300

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+let age = +prompt('yoshingizni kiriting');
+if(age <= 18) {
+  alert("yoshsiz. o'qishingiz kerak")
+}else if(age <= 50){
+  alert("ishlashingiz kerak")
+}else if(age <=59){
+  alert('Yaqinda pensiyaga chiqasiz')
+}else if(age <=150){
+  alert("Pensionerga o'xshaysiz, hali tirik bo'lsangiz")
+}else {
+  alert("Nimadir noto'g'ri ketti")
+}
+
+
